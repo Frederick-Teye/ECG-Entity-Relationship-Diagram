49387eb5d3d2de0d581ac2dc87c3bd0dd929c52c
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-04-01 10:04:47 +0000

    Add last three tables.
    The last three tables are billing_system, transmission_line, substation,
    and employee.

--- ecg.sql
+++ ecg.sql
@@ -28,8 +28,50 @@ CREATE TABLE transformer (
     transformer_id INT NOT NULL,
     location VARCHAR(255) NOT NULL,
     capacity FLOAT,
-    trans_line_id INT,
-    FOREIGN KEY (trans_line_id)
-        REFERENCES transmission_line (trans_line_id),
+    transmission_line_id INT,
+    FOREIGN KEY (transmission_line_id)
+        REFERENCES transmission_line (line_id),
     PRIMARY KEY (transformer_id)
 );
+
+CREATE TABLE billing_system (
+    invoice_number INT NOT NULL,
+    amount_due FLOAT NOT NULL,
+    billing_period TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    customer_id INT NOT NULL,
+    FOREIGN KEY (customer_id)
+        REFERENCES customer (customer_id),
+    PRIMARY KEY (invoice_number)
+);
+
+CREATE TABLE transmission_line (
+    line_id INT NOT NULL,
+    voltage_level FLOAT NOT NULL,
+    legnth FLOAT NOT NULL,
+    substation_id INT NOT NULL,
+    FOREIGN KEY (substation_id)
+        REFERENCES substation (station_id),
+    PRIMARY KEY (line_id)
+);
+
+CREATE TABLE substation (
+    station_id INT NOT NULL,
+    location VARCHAR(255),
+    capacity FLOAT,
+    PRIMARY KEY (station_id)
+);
+
+CREATE TABLE employee (
+    ssn INT NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    salary FLOAT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    sex VARCHAR(255),
+    job_role VARCHAR(255) NOT NULL,
+    substation INT NOT NULL,
+    FOREIGN KEY (substation)
+        REFERENCES substation (station_id),
+    PRIMARY KEY (snn)
+)
