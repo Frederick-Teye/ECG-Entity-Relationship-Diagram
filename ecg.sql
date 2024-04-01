860601688852b5f14bc6319cb2bb8e0d0c36467a
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2024-04-01 09:33:13 +0000

    Create meter table and transformer table

--- ecg.sql
+++ ecg.sql
@@ -10,3 +10,26 @@ CREATE TABLE customer (
     customer_type VARCHAR(255) NOT NULL,
     PRIMARY KEY (customer_id)
 );
+
+CREATE TABLE meter (
+    meter_id INT NOT NULL,
+    installation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    meter_type VARCHAR(255),
+    customer_id INT NOT NULL,
+    transformer_id INT NOT NULL,
+    FOREIGN KEY (customer_id)
+        REFERENCES customer (customer_id),
+    FOREIGN KEY (transformer_id)
+        REFERENCES transformer (transformer_id),
+    PRIMARY KEY (meter_id)
+);
+
+CREATE TABLE transformer (
+    transformer_id INT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    capacity FLOAT,
+    trans_line_id INT,
+    FOREIGN KEY (trans_line_id)
+        REFERENCES transmission_line (trans_line_id),
+    PRIMARY KEY (transformer_id)
+);
